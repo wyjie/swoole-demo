1332fc61dc75b37b6babbba01ac648c06a5dad05
arthur.zou <world_er@hotmail.com>
2018-08-08 16:37:45 +0800

    11

--- http_client.php
+++ http_client.php
@@ -3,7 +3,7 @@
 	$http = new swoole_http_server('0.0.0.0', 9501);
 
 	$http->on('request', function ($request, $response) {
-		$response->end('<h1>Hello, '. $request->get('name'); .'</h1>');
+		$response->end('<h1>Hello, '. $request->get('name') .'</h1>');
 	}); 
 
 	$http->start();
